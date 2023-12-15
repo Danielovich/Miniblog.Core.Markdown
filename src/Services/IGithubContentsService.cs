5791303ae35418db19be25bbc83dbdc443627ec7
Daniel Frost <danielfrostdk@outlook.com>
2023-12-15 16:25:58 +0100

    Added contract member

--- src/Services/IGithubContentsService.cs
+++ src/Services/IGithubContentsService.cs
@@ -1,11 +1,14 @@
-﻿namespace Miniblog.Core.Services
+namespace Miniblog.Core.Services
 {
     using Miniblog.Core.Models;
 
     using System.Collections.Generic;
+    using System.Threading.Tasks;
 
     public interface IGithubContentsService
     {
         IReadOnlyList<GitHubContentsApiResponse> GithubPosts { get; }
+
+        Task LoadGithubPostsAsync();
     }
 }
